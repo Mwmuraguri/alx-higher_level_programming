c7c4113a2e7b53b7e459bece633efb6b40c5ff06
Mwmuraguri <maryannewanjiku87@gmail.com>
2023-06-26 12:50:11 -0700

    Executes a function safely

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
+
 import sys
 
-try:
+def safe_function(fct, *args):
+    try:
         result = fct(*args)
-        return (result)
-    except:
-        print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
+        return result
+    except Exception as e:
+        print("Exception: {}".format(e), file=sys.stderr)
         return (None)
